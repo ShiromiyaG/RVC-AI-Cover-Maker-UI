55664f28c8a95a732df85f3b9086098e0919363a
Guilherme <86894155+ShiromiyaG@users.noreply.github.com>
2024-09-08 07:07:40 -0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -7,9 +7,31 @@ set REPO_URL=https://github.com/ShiromiyaG/RVC-AI-Cover-Maker-UI
 REM Navigate to the directory where the script is located
 cd /d %~dp0
 
-REM Loop through all directories except "env", logs" and "audio_files" and delete them
+REM Loop through all directories except "env", "logs", "audio_files", and "programs/applio_code/rvc/models"
 for /d %%D in (*) do (
-    if /i not "%%D"=="env" if /i not "%%D"=="logs" if /i not "%%D"=="audio_files" (
+    if /i not "%%D"=="env" if /i not "%%D"=="logs" if /i not "%%D"=="audio_files" if /i not "%%D"=="programs" (
+        echo Deleting directory %%D
+        rmdir /s /q "%%D"
+    )
+)
+
+REM Loop through all subdirectories in "programs" except "applio_code/rvc/models"
+for /d %%D in (programs\*) do (
+    if /i not "%%D"=="programs\applio_code" (
+        echo Deleting directory %%D
+        rmdir /s /q "%%D"
+    )
+)
+
+for /d %%D in (programs\applio_code\*) do (
+    if /i not "%%D"=="programs\applio_code\rvc" (
+        echo Deleting directory %%D
+        rmdir /s /q "%%D"
+    )
+)
+
+for /d %%D in (programs\applio_code\rvc\*) do (
+    if /i not "%%D"=="programs\applio_code\rvc\models" (
         echo Deleting directory %%D
         rmdir /s /q "%%D"
     )
@@ -17,7 +39,7 @@ for /d %%D in (*) do (
 
 REM Loop through all files and delete them
 for %%F in (*) do (
-    if not "%%F"=="updater.bat" (
+    if not "%%F"=="update.bat" (
         echo Deleting file %%F
         del /q "%%F"
     )
